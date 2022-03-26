f18fe60a0eaa8fd888c54d1305316e3ae26d82f1
coreyfedde <coreysfedde@gmail.com>
2022-03-25 21:02:32 -0400

    time periods roughly working

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SimpleBarChart from './components/charts/SimpleBarChart';
 import StackedBarChart from './components/charts/StackedBarChart';
 import SimpleLineChart from './components/charts/SimpleLineChart';
 import TimeSlider from './components/TimeSlider';
+import TimePeriod from './components/TimePeriod';
 
 const calculateInterestOverYear = (principal, rate) => {
   return principal * rate;
@@ -47,29 +48,91 @@ function App() {
       monthlyContribution: 200,
       rate: .08,
   })
+  const [timeRangeValues, setTimeRangeValues] = useState([{start: userValues.age, end: userValues.retirementTarget, period: 1}])
   const investmentYears = userValues.retirementTarget - userValues.age;
 
   const financialData = calculateTotalRetirementWithMonthlyContribution(investmentYears, userValues.monthlyContribution, userValues.deposit, userValues.rate);
 
   const timeScale = Array.from({length: investmentYears}, (v, i) => new Date().getFullYear() + i)
 
-const handleChange = (event) => {
+const handleUserValueChange = (event) => {
   setUserValues((values) => ({
     ...values,
     [event.target.name]: Number(event.target.value),
   }));
 }
 
+// MOVE THIS TO CHANGE STATE WITHIN COMPONENT AND THEN SUBMIT AFTER CLICKING OFF AN INPUT TO THIS STATE
+const handleTimeRangeChange = e => {
+  // COULD USE DEFAULTVALUE INSTEAD OF PLACEHOLDERS
+  // Set the targeted value
+  // Then set the subsequent values that need to be adjusted
+  const {name, value, placeholder} = e.target;
+  console.log('name', name);
+  console.log('value', value);
+  console.log('defaultValue', placeholder)
+  const targetRange = timeRangeValues.findIndex(r => r[name] === Number(placeholder));
+  const cloneArr = timeRangeValues.slice();
+  if (name === 'start') {
+    if (value < userValues.age) {
+      return;
+    }
+    cloneArr[targetRange][name] = Number(value);
+  }
+  if (name === 'end') {
+    if (value > userValues.retirementTarget) {
+      return;
+    }
+    if (cloneArr.length > 1) {
+      cloneArr[targetRange + 1].start = Number(value) + 1;
+    }
+    cloneArr[targetRange][name] = Number(value);
+  }
+  setTimeRangeValues(cloneArr)
+}
+
+const addTimeRange = e => {
+  const previousLastRange = timeRangeValues[timeRangeValues.length - 1];
+  const currentRange = previousLastRange.end - previousLastRange.start;
+  const newRange = currentRange / 2;
+  const newEnd = Math.floor(previousLastRange.start + newRange);
+  const newStart = Math.floor(previousLastRange.start + newRange + 1);
+  const newTimeRangeValues = [...timeRangeValues.length === 1 ? [] : timeRangeValues.slice(0, timeRangeValues.length - 1), {start: previousLastRange.start, end: newEnd, period: previousLastRange.period}, {start: newStart, end: previousLastRange.end, period: previousLastRange.period + 1}]
+  return setTimeRangeValues(newTimeRangeValues)
+}
+const removeTimeRange = e => {
+  if (timeRangeValues.length === 2) {
+    return setTimeRangeValues([{start: userValues.age, end: userValues.retirementTarget, period: 1}]);
+  }
+  const previousLastRange = timeRangeValues[timeRangeValues.length - 2];
+  const currentRange = previousLastRange.end - previousLastRange.start;
+  const newTimeRangeValues = [...timeRangeValues.slice(0, timeRangeValues.length - 2), {start: previousLastRange.start, end: userValues.retirementTarget, period: previousLastRange.period}]
+  return setTimeRangeValues(newTimeRangeValues)
+}
+
+const lastTimeRangeValue = timeRangeValues[timeRangeValues.length - 1];
+const lastTimeRange = lastTimeRangeValue.end - lastTimeRangeValue.start;
+const shouldShowAddTimeRangeButton = Boolean(lastTimeRange > 1);
+const shouldShowRemoveTimeRangeButton = Boolean(timeRangeValues.length > 1)
+
+console.log('timeranges', timeRangeValues)
+
   return (
     <div style={{display: 'flex'}}>
       <div style={{flex: 1}}>
-        <InfoForm userValues={userValues} handleChange={handleChange}/>
+        <InfoForm userValues={userValues} handleChange={handleUserValueChange}/>
+        {shouldShowAddTimeRangeButton && <button onClick={addTimeRange}>Add Time Period </button>}
+        {shouldShowRemoveTimeRangeButton && <button onClick={removeTimeRange}> Remove Time Period</button>}
+        ____
+        {timeRangeValues.map(r => <TimePeriod start={r.start} end={r.end} period={r.period} userValues={userValues} handleChange={handleTimeRangeChange} />)}
+        
       </div>
       <div style={{flex: 1}}>
         <div>Final: {financialData[financialData.length - 1]?.total}</div>
-        <DonutChart data={financialData} />
+        {!!financialData.length && 
+        <div>
         <StackedBarChart data={financialData} time={timeScale}/>
-        <SimpleLineChart data={financialData} time={timeScale} />
+        </div>}
       </div>
     </div>
   );
---
+++ src/components/TimePeriod.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import '../App.css';
+
+const RangeInput = ({start, end, period, handleChange, userValues}) => {
+  const {age, retirementTarget, deposit, monthlyContribution, rate} = userValues;
+  return (
+    <div style={{margin: '15px auto 25px'}}>
+        <div style={{display: 'flex'}}>
+           <div>Period {period}</div> <input type="text" value={start} name='start' onChange={handleChange}/> - <input type="text" value={end} name='end' onChange={handleChange}/>
+        </div>
+    </div>
+  );
+}
+
+{/* <div>
+<div style={{display: 'flex'}}>
+    {start} - {end}
+</div>
+<div>
+<label>Monthly Contribution</label><input type="text" value={monthlyContribution} name="monthlyContribution" onChange={handleChange}/>
+</div>
+<div>
+<label>Expected Rate of Return</label><input type="text" value={rate} name="rate" onChange={handleChange}/>
+</div>
+</div> */}
+
+export default RangeInput;
